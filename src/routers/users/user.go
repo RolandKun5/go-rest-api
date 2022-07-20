6d003c8d2c542206af1ef50a7058da715994f7c5
rolandkun <roland.kun98@gmail.com>
2022-07-20 13:02:10 +0200

    init user routers

---
+++ src/routers/users/user.go
@@ -0,0 +1,10 @@
+package users
+
+import (
+	"github.com/RolandKun5/go-rest-api/src/routers/users/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func Init(router *gin.Engine) {
+	router.GET("/users", handlers.GetUsers)
+}
