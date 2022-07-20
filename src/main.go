be7355f2425b99131323ddc6e02cecc730f93335
rolandkun <roland.kun98@gmail.com>
2022-07-20 13:01:53 +0200

    add server

--- src/main.go
+++ src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/RolandKun5/go-rest-api/src/database"
+	"github.com/RolandKun5/go-rest-api/src/server"
 )
 
 func main() {
 	database.Init()
+	server.Start()
 }
