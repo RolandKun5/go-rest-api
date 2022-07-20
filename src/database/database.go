8b368d8f8487302965f48e8ccb10078f0a72cc10
rolandkun <roland.kun98@gmail.com>
2022-07-20 10:07:13 +0200

    init database.go

--- src/database/database.go
+++ src/database/database.go
@@ -1,1 +1,58 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var Db *sql.DB
+
+type connection struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+}
+
+func Init() {
+	err := godotenv.Load("/.env")
+
+	if err != nil {
+		fmt.Printf("Error loading .env file: %s\n", err.Error())
+		return
+	}
+
+	connInfo := connection{
+		Host:     os.Getenv("POSTGRES_URL"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:   os.Getenv("POSTGRES_DB"),
+	}
+
+	Db, err = sql.Open("postgres", connToString(connInfo))
+
+	if err != nil {
+		fmt.Printf("Error connecting to the DB: %s\n", err.Error())
+		return
+	} else {
+		fmt.Printf("DB is open\n")
+	}
+
+	err = Db.Ping()
+	if err != nil {
+		fmt.Printf("Error could not ping database: %s\n", err.Error())
+		return
+	} else {
+		fmt.Printf("DB pinged successfully\n")
+	}
+}
+
+func connToString(info connection) string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		info.Host, info.Port, info.User, info.Password, info.DBName)
+}
